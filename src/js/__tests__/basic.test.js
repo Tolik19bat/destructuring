766099d2be0ef1ab839d25d45cf0ac3800906f7e
tolik19bat <play.hoffa@gmail.com>
2023-11-19 23:23:51 +0300

    end_work_code_commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -5,3 +5,47 @@ test('should sum', () => {
 
   expect(result).toBe(6);
 });
+
+import showDescription from '../app';+
+
+test('check description', () => {
+
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const result = showDescription(character);
+
+  expect(result).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,39 @@
-// TODO: write your code here
+// // TODO: write your code here
 import sum from './basic';
 
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+// const character = {
+//   name: 'Лучник',
+//   type: 'Bowman',
+//   health: 50,
+//   level: 3,
+//   attack: 40,
+//   defence: 10,
+//   special: [
+//     {
+//       id: 8,
+//       name: 'Двойной выстрел',
+//       icon: 'http://...',
+//       description: 'Двойной выстрел наносит двойной урон'
+//     },
+//     {
+//       id: 9,
+//       name: 'Нокаутирующий удар',
+//       icon: 'http://...'
+//       // <- обратите внимание, описание "засекречено"
+//     }
+//   ]
+// }
+
+export default function showDescription({ special: [...restObj] }) {
+  for (let i = 0; i <= restObj.length - 1; i++) {
+    if (!('description' in restObj[i])) {
+      restObj[i].description = 'Описание недоступно';
+    }
+  }
+  console.log(restObj)
+  return restObj;
+}
